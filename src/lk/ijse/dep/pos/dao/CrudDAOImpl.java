4f2981512b6788ce23184bd53d2cad25e6fe073f
Kaweesha De Alwis <kaweesha12lk@gmail.com>
2019-11-19 14:33:22 +0530

    Done JPA

---
+++ src/lk/ijse/dep/pos/dao/CrudDAOImpl.java
@@ -0,0 +1,52 @@
+package lk.ijse.dep.pos.dao;
+
+import lk.ijse.dep.pos.entity.SuperEntity;
+import org.hibernate.Session;
+
+import javax.persistence.EntityManager;
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+
+public abstract class CrudDAOImpl<T extends SuperEntity, ID extends Serializable> implements CrudDAO<T, ID> {
+
+    private Class<T> entity;
+    protected EntityManager entityManager;
+
+    public CrudDAOImpl() {
+        entity = (Class<T>) (((ParameterizedType) (this.getClass().getGenericSuperclass())).getActualTypeArguments()[0]);
+    }
+
+    @Override
+    public void setEntityManager(EntityManager entiryManager) {
+        this.entityManager = entiryManager;
+
+    }
+
+
+    @Override
+    public List<T> findAll() throws Exception {
+        return entityManager.createQuery("SELECT c FROM " + entity.getName()+" c").getResultList();
+    }
+
+    @Override
+    public T find(ID id) throws Exception {
+        return entityManager.getReference(entity, id);
+    }
+
+    @Override
+    public void save(T entity) throws Exception {
+        entityManager.persist(entity);
+    }
+
+    @Override
+    public void update(T entity) throws Exception {
+        entityManager.merge(entity);
+    }
+
+    @Override
+    public void delete(ID id) throws Exception {
+        entityManager.remove(entityManager.find(entity, id));
+
+    }
+}
--- src/lk/ijse/dep/pos/dao/custom/impl/OrderDAOImpl.java
+++ src/lk/ijse/dep/pos/dao/custom/impl/OrderDAOImpl.java
@@ -1,66 +1,28 @@
 package lk.ijse.dep.pos.dao.custom.impl;
 
-import lk.ijse.dep.pos.dao.CrudUtil;
+import lk.ijse.dep.pos.dao.CrudDAOImpl;
+import lk.ijse.dep.pos.dao.custom.CustomerDAO;
 import lk.ijse.dep.pos.dao.custom.OrderDAO;
+import lk.ijse.dep.pos.entity.Customer;
 import lk.ijse.dep.pos.entity.Order;
 
+import javax.persistence.EntityManager;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
 
-public class OrderDAOImpl implements OrderDAO {
+public class OrderDAOImpl extends CrudDAOImpl<Order, String> implements OrderDAO {
+
+    private EntityManager entityManager;
 
     @Override
     public int getLastOrderId() throws Exception {
-        ResultSet rst = CrudUtil.execute("SELECT id FROM `Order` ORDER BY id DESC LIMIT 1");
-        if (rst.next()) {
-            return rst.getInt(1);
-        } else {
-            return 0;
-        }
+        return (Integer) entityManager.createNativeQuery("SELECT id FROM `Order` ORDER BY id DESC LIMIT 1").getSingleResult();
+
     }
 
     @Override
     public boolean existsByCustomerId(String customerId) throws Exception {
-        ResultSet rst = CrudUtil.execute("SELECT * FROM `Order` WHERE customerId=?", customerId);
-        return rst.next();
+        return entityManager.createNativeQuery("SELECT * FROM `Order` WHERE id=?1")
+                .setParameter(1, customerId).getResultList() != null;
     }
 
-    @Override
-    public List<Order> findAll() throws Exception {
-        ResultSet rst = CrudUtil.execute("SELECT * FROM `Order`");
-        List<Order> orders = new ArrayList<>();
-        while (rst.next()){
-            orders.add(new Order(rst.getInt(1),
-                    rst.getDate(2),
-                    rst.getString(3)));
-        }
-        return orders;
-    }
-
-    @Override
-    public Order find(Integer orderId) throws Exception {
-        ResultSet rst =CrudUtil.execute("SELECT * FROM `Order` WHERE id=?", orderId);
-        if (rst.next()){
-            return new Order(rst.getInt(1),
-                    rst.getDate(2),
-                    rst.getString(3));
-        }
-        return null;
-    }
-
-    @Override
-    public boolean save(Order order) throws Exception {
-        return CrudUtil.execute("INSERT INTO `Order` VALUES (?,?,?)", order.getId(), order.getDate(), order.getCustomerId());
-    }
-
-    @Override
-    public boolean update(Order order) throws Exception {
-        return CrudUtil.execute("UPDATE `Order` SET name=?, address=? WHERE id=?", order.getDate(), order.getCustomerId(), order.getId());
-    }
-
-    @Override
-    public boolean delete(Integer orderId) throws Exception {
-        return CrudUtil.execute("DELETE FROM `Order` WHERE id=?", orderId);
-    }
 }
