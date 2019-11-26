e19fd7594c3dc78c00adce0f4da0c9a4d87d11e8
Kaweesha De Alwis <kaweesha12lk@gmail.com>
2019-11-26 11:15:05 +0530

    Add Transactional Annotation to BO layer

--- src/lk/ijse/dep/pos/business/custom/impl/OrderBOImpl.java
+++ src/lk/ijse/dep/pos/business/custom/impl/OrderBOImpl.java
@@ -10,11 +10,13 @@ import lk.ijse.dep.pos.entity.*;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Transactional
 @Component
 public class OrderBOImpl implements OrderBO {
 
@@ -37,16 +39,9 @@ public class OrderBOImpl implements OrderBO {
     @Override
     public void placeOrder(OrderDTO order) throws Exception {
 
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            itemDAO.setSession(session);
-            orderDAO.setSession(session);
-            orderDetailDAO.setSession(session);
-            customerDAO.setSession(session);
-            session.beginTransaction();
-
             int oId = order.getId();
             orderDAO.save(new Order(oId, new java.sql.Date(new Date().getTime()),
-                    session.load(Customer.class, order.getCustomerId())));
+                    customerDAO.find(order.getCustomerId())));
 
             for (OrderDetailDTO orderDetail : order.getOrderDetails()) {
                 orderDetailDAO.save(new OrderDetail(oId, orderDetail.getCode(),
@@ -58,24 +53,17 @@ public class OrderBOImpl implements OrderBO {
 
             }
 
-            session.getTransaction().commit();
-
-        }
     }
 
     @Override
     public List<OrderDTO2> getOrderInfo(String query) throws Exception {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            queryDAO.setSession(session);
-            session.beginTransaction();
             List<CustomEntity> ordersInfo = queryDAO.getOrdersInfo(query + "%");
             List<OrderDTO2> dtos = new ArrayList<>();
             for (CustomEntity info : ordersInfo) {
                 dtos.add(new OrderDTO2(info.getOrderId(),
                         info.getOrderDate(), info.getCustomerId(), info.getCustomerName(), info.getOrderTotal()));
             }
-            session.getTransaction().commit();
             return dtos;
-        }
+
     }
 }
